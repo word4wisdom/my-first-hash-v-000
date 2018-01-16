c342b0d774351bd6d3afa97577a44474ceb66663
Joan' Araujo <wisdombthegame@gmail.com>
2018-01-16 09:49:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,9 +10,15 @@ end
 
 def shipping_manifest
   the_manifest = {
+<<<<<<< HEAD
       "whale bone corsets" => 5,
       "oil paintings" => 2,
       "porcelain vases" => 3
+=======
+      :whale_bones => 5
+      :porcelain_vases => 3
+      :oil_paintings => 3
+>>>>>>> 5e954319c99bb9619ac44108122af3de1582c88a
   }
 
 end
@@ -23,7 +29,11 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+<<<<<<< HEAD
                  shipping_manifest["oil paintings"]
+=======
+                 shipping_manifest["oil_paintings"]
+>>>>>>> 5e954319c99bb9619ac44108122af3de1582c88a
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
